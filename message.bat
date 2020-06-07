271f5d0ba3e088351c49dcd03959596900354194
wowexec <57664756+wowexec@users.noreply.github.com>
2020-06-07 11:46:42 +0800

    Create message.bat

---
+++ message.bat
@@ -0,0 +1,27 @@
+@echo off
+set /p name=Content:
+set /p sip=IP Address:
+
+>nul2>&1"%SYSTEMROOT%\system32\cacls.exe""%SYSTEMROOT%\system32\config\system"
+
+if '%errorlevel%' NEQ '0' (goto UACPrompt) else (goto gotAdmin)
+
+:UACPrompt
+
+echo SetUAC=CreateObject^("Shell.Application"^)>"%temp%\getadmin.vbs"
+
+echo UAC.ShellExecute"%~s0","","","runas",1>>"%temp%\getadmin.vbs"
+
+"%temp%\getadmin.vbs"
+
+exit /b
+
+:gotAdmin
+
+if exist "%temp%\getadmin.vbs" (del"%temp%\getadmin.vbs")
+
+//set name=%~n0
+
+MSG /server:%sip% * %name%
+
+MSG /server:127.0.0.1 * "Send successfully!"
